840d1101b0c80bb6b00c1e9aec84bfb2ba476b54
imnotrafa <abreurafa22@gmail.com>
2023-10-14 01:55:17 -0400

    Dynamically added names to images and links

--- src/Pages/Eboard.js
+++ src/Pages/Eboard.js
@@ -5,20 +5,39 @@ import eboard from "./eboard_style.module.css";
 
 // This function gets all values from the folder
 const images = require.context("../Eboard-Photos", true);
-
 // what does the .keys().map() do?
-const imageList = images.keys().map((image) => images(image));
+const imageList = images.keys().map((image) => {
+  const img_url =images(image)
+  //Just get the names of each image added
+  const img_name = image.split('/').pop().replace(/\.jpg$/,'');
+  return {url:img_url,name: img_name};
+});
+  
+const linkedins_and_names = {
+  "Rafael":{
+  Position:"Publics Relations",
+  Linkedin: "https://www.linkedin.com/in/rafael--abreu/",
+},
+  "Michelle":{
+  FirstName:"President",
+  Linkedin: "https://www.linkedin.com/in/michelleabautista/"
+},
+}
 const Eboard = () => {
   return (
     <>
       <h1>Meet Our Eboard 2023-2024</h1>
       <div className={eboard.container}>
-        {imageList.map((image, index) => (
-          <img src={image} className={eboard.main_img} alt="pic"></img>
-        ))}
+          {imageList.map((image, index) => (
+            <div key={index}>
+              <h1>{image.name}</h1>
+              
+              <a href={linkedins_and_names[image.name]}><img src={image.url} className={eboard.main_img} alt="pic"></img></a>
+            </div>
+          ))}
       </div>
     </>
   );
 };
-//Make a function that style each photo of the page
+//make sure that links are adding dynamically to each image based on the names
 export default Eboard;
