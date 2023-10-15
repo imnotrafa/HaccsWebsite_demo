2f4925476144d85f426ccc5de5909afe52ffabbe
imnotrafa <abreurafa22@gmail.com>
2023-10-14 21:55:57 -0400

    Photos needs to be re arranged

--- src/Pages/Eboard.js
+++ src/Pages/Eboard.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+//import Button from "react-bootstrap/Button";
+//import Card from "react-bootstrap/Card";
 import eboard from "./eboard_style.module.css";
+import { linearProgressClasses } from "@mui/material";
 
 // This function gets all values from the folder
 const images = require.context("../Eboard-Photos", true);
+console.log(images.keys())
 // what does the .keys().map() do?
 const imageList = images.keys().map((image) => {
   const img_url =images(image)
@@ -19,20 +21,35 @@ const linkedins_and_names = {
   Linkedin: "https://www.linkedin.com/in/rafael--abreu/",
 },
   "Michelle":{
-  FirstName:"President",
+  Position:"President",
   Linkedin: "https://www.linkedin.com/in/michelleabautista/"
 },
-}
+  "Leslie":{
+    Position:"Vice-President",
+    Linkedin:"https://www.linkedin.com"
+
+  },
+  "Jason":{
+    Position:"Educational coordinator",
+    Linkedin:"https://www.linkedin.com/in/jason-morales-d/"
+  },
+  "Gabriel":{
+    Position:"Secretary",
+    Linkedin:"https://www.linkedin.com"
+  }
+};
 const Eboard = () => {
   return (
     <>
-      <h1>Meet Our Eboard 2023-2024</h1>
+      <h1 className={eboard.haccs}>Meet Our Eboard 2023-2024</h1>
       <div className={eboard.container}>
           {imageList.map((image, index) => (
-            <div key={index}>
-              <h1>{image.name}</h1>
-              
-              <a href={linkedins_and_names[image.name]}><img src={image.url} className={eboard.main_img} alt="pic"></img></a>
+            <div>
+              <h1 id="Position">{linkedins_and_names[image.name].Position}</h1>
+              <div>
+                <a className={eboard.link} href={linkedins_and_names[image.name].Linkedin} target="_blank"><img src={image.url} className={eboard.main_img} alt="pic"></img></a>
+              </div>
+              <p id="Name">{image.name}</p>
             </div>
           ))}
       </div>
