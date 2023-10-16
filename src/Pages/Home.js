c878bb0af93823c97fd51306308edec0c9bcc294
imnotrafa <abreurafa22@gmail.com>
2023-10-16 15:58:45 -0400

    Text changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,4 @@
+import logo from "../HACCS_Logo.png";
 import DanielWin from "../CarouselPics/DanielWin.JPG";
 import Lim_Pres from "../CarouselPics/Lim_Pres.jpg";
 import firstGBMFall from "../CarouselPics/firstGBMFall.jpg";
@@ -7,7 +8,8 @@ import KeyboardDoubleArrowRightRounded from "@mui/icons-material/KeyboardDoubleA
 import KeyboardDoubleArrowLeftRounded from "@mui/icons-material/KeyboardDoubleArrowLeftRounded";
 import "react-alice-carousel/lib/alice-carousel.css";
 import "../carousel.css";
-
+import home from "./home_style.module.css"
+import Jason from "../Eboard-Photos/Jason.jpg"
 const HomePage = () => {
   return (
     <>
@@ -36,24 +38,48 @@ const HomePage = () => {
           <img src={InvolvementFair} className="sliderimg" alt="pic" />
         </AliceCarousel>
       </div>
-      <div className="MandV">
-        <h1>Mission</h1>
-        <p>
-          To enhance the college experience and professional development of
-          Hispanic/Latino students in the Ying Wu College of Computing (YWCC) by
-          providing resources such as workshops, technical interview prep, and
-          fostering a supportive community. HACCS promotes career advancement,
-          facilitates information exchange, and celebrates cultural diversity.
-        </p>
+      <div className={home.info}>
 
-        <h1>Vision</h1>
-        <p>
-          We are committed to cultivating a strong community, encouraging
-          cultural awareness, and providing resources to empower members as they
-          pursue successful careers in computing. Through collaboration and
-          engagement, we aim to amplify our impact and contribute to a more
-          inclusive and diverse computing field.
-        </p>
+        <div className={home.mission}>
+          <h1>Mission</h1>
+          <div className={home.missionText}>
+            <p>
+              To enhance the college experience and professional development of
+              Hispanic/Latino students in the Ying Wu College of Computing (YWCC) by
+              providing resources such as workshops, technical interview prep, and
+              fostering a supportive community. HACCS promotes career advancement,
+              facilitates information exchange, and celebrates cultural diversity.
+            </p>
+          </div>
+
+        </div>
+        <div className={home.vision}>
+          <h1>Vision</h1>
+          <div className={home.visionText}>
+            <img></img>
+            <p>
+              We are committed to cultivating a strong community, encouraging
+              cultural awareness, and providing resources to empower members as they
+              pursue successful careers in computing. Through collaboration and
+              engagement, we aim to amplify our impact and contribute to a more
+              inclusive and diverse computing field.
+            </p>
+          </div>
+
+        </div>
+        
+      </div>
+      <div className="contact">
+        <img className="contact-logo" src={logo} alt="HACCS Logo"></img>
+        <h3>Contact Us</h3>
+        <div className="txt_below">
+          <p>njithaccs@gmail.com</p>
+        </div>
+        <h3 className="contact">Follow Us</h3>
+
+        <div className="txt_below">
+          <p> Discord Instagram</p>
+        </div>
       </div>
     </>
   );
