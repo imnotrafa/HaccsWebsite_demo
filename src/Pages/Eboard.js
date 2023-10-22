fb15a09c48ddfa2a45fc366bb397c213ef6d4783
imnotrafa <abreurafa22@gmail.com>
2023-10-22 03:58:58 -0400

    Rearanged the eboard, no more dynamic content (images hardcoded by index), if image added positions will change

--- src/Pages/Eboard.js
+++ src/Pages/Eboard.js
@@ -5,7 +5,6 @@ import eboard from "./eboard_style.module.css";
 
 // This function gets all values from the folder
 const images = require.context("../Eboard-Photos", true);
-console.log(images.keys());
 // what does the .keys().map() do?
 const imageList = images.keys().map((image) => {
   const img_url = images(image);
@@ -42,28 +41,54 @@ const linkedins_and_names = {
 const Eboard = () => {
   return (
     <>
-      console.log(image.name);
       <h1 className={eboard.haccs}>Meet Our Eboard 2023-2024</h1>
+      <div className={eboard.President}>
+        <h1 id="Position">{linkedins_and_names[imageList[3].name].Position}</h1>
+        <div>
+          <a className={eboard.link} href={linkedins_and_names[imageList[3].name].Linkedin} target="_blank">
+                    <img src={imageList[3].url} className={eboard.main_img}
+                    alt="pic"></img></a>
+        </div>
+        <p id="Name">{imageList[3].name}</p>
+      </div>
       <div className={eboard.container}>
-        {imageList.map((image, index) => (
-          <div>
-            <h1 id="Position">{linkedins_and_names[image.name].Position}</h1>
-            <div>
-              <a
-                className={eboard.link}
-                href={linkedins_and_names[image.name].Linkedin}
-                target="_blank"
-              >
-                <img
-                  src={image.url}
-                  className={eboard.main_img}
-                  alt="pic"
-                ></img>
-              </a>
-            </div>
-            <p id="Name">{image.name}</p>
-          </div>
-        ))}
+      <div className={eboard.Vice}>
+        <h1 id="Position">{linkedins_and_names[imageList[2].name].Position}</h1>
+        <div>
+          <a className={eboard.link} href={linkedins_and_names[imageList[2].name].Linkedin} target="_blank">
+                    <img src={imageList[2].url} className={eboard.main_img}
+                    alt="pic"></img></a>
+        </div>
+        <p id="Name">{imageList[2].name}</p>
+      </div>
+      <div className={eboard.Secretary}>
+        <h1 id="Position">{linkedins_and_names[imageList[0].name].Position}</h1>
+        <div>
+          <a className={eboard.link} href={linkedins_and_names[imageList[0].name].Linkedin} target="_blank">
+                    <img src={imageList[0].url} className={eboard.main_img}
+                    alt="pic"></img></a>
+        </div>
+        <p id="Name">{imageList[0].name}</p>
+      </div>
+      <div className={eboard.PR}>
+        <h1 id="Position">{linkedins_and_names[imageList[4].name].Position}</h1>
+        <div>
+          <a className={eboard.link} href={linkedins_and_names[imageList[4].name].Linkedin} target="_blank">
+                    <img src={imageList[4].url} className={eboard.main_img}
+                    alt="pic"></img></a>
+        </div>
+        <p id="Name">{imageList[4].name}</p>
+      </div>
+      <div className={eboard.President}>
+        <h1 id="Position">{linkedins_and_names[imageList[1].name].Position}</h1>
+        <div>
+          <a className={eboard.link} href={linkedins_and_names[imageList[1].name].Linkedin} target="_blank">
+                    <img src={imageList[1].url} className={eboard.main_img}
+                    alt="pic"></img></a>
+        </div>
+        <p id="Name">{imageList[1].name}</p>
+      </div>
+      
       </div>
     </>
   );
