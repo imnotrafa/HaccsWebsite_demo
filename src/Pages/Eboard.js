dff3d89870ca7d8638884bb03b532ca7778b9207
imnotrafa <abreurafa22@gmail.com>
2023-11-13 02:04:13 -0500

    Create and refactor new grid for eboard images, scaling of images should be fixed

--- src/Pages/Eboard.js
+++ src/Pages/Eboard.js
@@ -52,75 +52,73 @@ const Eboard = () => {
   return (
     <>
       <h1 className={eboard.haccs}>Meet Our Eboard 2023-2024</h1>
-      <div className={eboard.President}>
-        <h1 id="Position">{linkedins_and_names[imageList[4].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[4].name].Linkedin} target="_blank">
-                    <img src={imageList[4].url} className={eboard.main_img}
-                    alt="pic"></img></a>
+      <div className={eboard.main_container}>
+        <div className={eboard.member}>
+          <h1 id="Position">{linkedins_and_names[imageList[4].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[4].name].Linkedin} target="_blank">
+                      <img src={imageList[4].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[4].name}</p>
+        </div>
+        <div className={eboard.member}>
+          <h1 id="Position">{linkedins_and_names[imageList[3].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[3].name].Linkedin} target="_blank">
+                      <img src={imageList[3].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[3].name}</p>
+        </div>
+        <div className={eboard.Secretary}>
+          <h1 id="Position">{linkedins_and_names[imageList[1].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[1].name].Linkedin} target="_blank">
+                      <img src={imageList[1].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[1].name}</p>
+        </div>
+        <div className={eboard.member}>
+          <h1 id="Position">{linkedins_and_names[imageList[0].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[0].name].Linkedin} target="_blank">
+                      <img src={imageList[0].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[0].name}</p>
+        </div>
+        <div className={eboard.member}>
+          <h1 id="Position">{linkedins_and_names[imageList[2].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[2].name].Linkedin} target="_blank">
+                      <img src={imageList[2].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[2].name}</p>
         </div>
 
-        <p id="Name" className={eboard.name}>{imageList[4].name}</p>
-      </div>
-      <div className={eboard.container}>
-      <div className={eboard.Vice}>
-        <h1 id="Position">{linkedins_and_names[imageList[3].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[3].name].Linkedin} target="_blank">
-                    <img src={imageList[3].url} className={eboard.main_img}
-                    alt="pic"></img></a>
+        <div className={eboard.member}>
+          <h1 id="Position">{linkedins_and_names[imageList[6].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[6].name].Linkedin} target="_blank">
+                      <img src={imageList[6].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[6].name}</p>
         </div>
-        <p id="Name" className={eboard.name}>{imageList[3].name}</p>
-      </div>
-      <div className={eboard.Secretary}>
-        <h1 id="Position">{linkedins_and_names[imageList[1].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[1].name].Linkedin} target="_blank">
-                    <img src={imageList[1].url} className={eboard.main_img}
-                    alt="pic"></img></a>
+        <div className={eboard.member}>
+          <h1 id="Position">{linkedins_and_names[imageList[5].name].Position}</h1>
+          <div>
+            <a className={eboard.link} href={linkedins_and_names[imageList[5].name].Linkedin} target="_blank">
+                      <img src={imageList[5].url} className={eboard.main_img}
+                      alt="pic"></img></a>
+          </div>
+          <p id="Name" className={eboard.name}>{imageList[5].name}</p>
         </div>
-        <p id="Name" className={eboard.name}>{imageList[1].name}</p>
-      </div>
-      <div className={eboard.PR}>
-        <h1 id="Position">{linkedins_and_names[imageList[0].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[0].name].Linkedin} target="_blank">
-                    <img src={imageList[0].url} className={eboard.main_img}
-                    alt="pic"></img></a>
-        </div>
-        <p id="Name" className={eboard.name}>{imageList[0].name}</p>
-      </div>
-      <div className={eboard.President}>
-        <h1 id="Position">{linkedins_and_names[imageList[2].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[2].name].Linkedin} target="_blank">
-                    <img src={imageList[2].url} className={eboard.main_img}
-                    alt="pic"></img></a>
-        </div>
-        <p id="Name" className={eboard.name}>{imageList[2].name}</p>
-      </div>
-
-      <div className={eboard.President}>
-        <h1 id="Position">{linkedins_and_names[imageList[6].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[6].name].Linkedin} target="_blank">
-                    <img src={imageList[6].url} className={eboard.main_img}
-                    alt="pic"></img></a>
-        </div>
-        <p id="Name" className={eboard.name}>{imageList[6].name}</p>
-      </div>
-
-      <div className={eboard.President}>
-        <h1 id="Position">{linkedins_and_names[imageList[5].name].Position}</h1>
-        <div>
-          <a className={eboard.link} href={linkedins_and_names[imageList[5].name].Linkedin} target="_blank">
-                    <img src={imageList[5].url} className={eboard.main_img}
-                    alt="pic"></img></a>
-        </div>
-        <p id="Name" className={eboard.name}>{imageList[5].name}</p>
       </div>
       
-      </div>
     </>
   );
 };
